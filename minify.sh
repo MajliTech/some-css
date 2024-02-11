6185b28d8b57fcd70f5aec45314a2302c9057e35
Miłosz Thiede <git@majlitech.pl>
2024-02-11 14:03:28 +0100

    add 2 headers: to minified and to script

--- minify.sh
+++ minify.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+# /*            Some CSS: https://github.com/MajliTech/some-css            */
+# /*                     Copyright (C) 2024 MajliTech                      */
+# /*                                                                       */
+# /* This program is free software: you can redistribute it and/or modify  */
+# /* it under the terms of the GNU General Public License as published by  */
+# /*   the Free Software Foundation, either version 3 of the License, or   */
+# /*                  (at your option) any later version.                  */
+# /*    This program is distributed in the hope that it will be useful,    */
+# /*    but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+# /*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     */
+# /*             GNU General Public License for more details.              */
+# /*   You should have received a copy of the GNU General Public License   */
+# /* along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
+
 mkdir -p min-css
 for file in css/*.css; do
   filename=$(basename "$file" .css)
-  minify "$file" > "min-css/$filename.min.css"
+  minified_css = "/*            Some CSS: https://github.com/MajliTech/some-css            */
+/*                     Copyright (C) 2024 MajliTech                      */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/*   the Free Software Foundation, either version 3 of the License, or   */
+/*                  (at your option) any later version.                  */
+/*    This program is distributed in the hope that it will be useful,    */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     */
+/*             GNU General Public License for more details.              */
+/*   You should have received a copy of the GNU General Public License   */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>.*/\n\n$(minify $file)"
   echo "Minified '$file' to 'min-css/$filename.min.css'"
 done
